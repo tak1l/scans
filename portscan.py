fd9e05b4f2c6483db9f2e537f5aae1147cdf4418
Caio Sonomura <caiosonomura@gmail.com>
2019-03-30 22:17:34 -0300

    Add portscan.py

---
+++ portscan.py
@@ -0,0 +1,12 @@
+import socket
+
+ports = [21,23,80,443,8080]
+
+for port in ports:
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.settimeout(0.1)
+    codigo = client.connect_ex(('127.0.0.1', port))
+    if codigo == 0:
+        print "PORT:\n",port,"OPEN"
+    else:
+        print "PORT:\n",port, "CLOSED"
